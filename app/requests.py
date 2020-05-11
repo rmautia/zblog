5279711e7ca83e874fbb3cfa887645aa76ec0c7a
rmautia <raphaelnyangenya@gmail.com>
2020-05-11 03:05:52 +0300

    class collecting quotes information

--- app/requests.py
+++ app/requests.py
@@ -4,3 +4,21 @@ from .models import quote
 
 Quote = quote.Quote
 quote_url = app.config['QUOTES_API_URL']
+
+def get_quote():
+    '''
+    Function that gets the json response to our url request and returns a random quote
+    '''
+    with urllib.request.urlopen(quote_url) as url:
+        get_quote_data = url.read()
+        get_quote_response = json.loads(get_quote_data)
+
+        quote_results = None
+
+        if get_quote_response:
+            author = get_quote_response.get('author')
+            quote = get_quote_response.get('quote')
+
+            quote_results = Quote(author, quote)
+    
+    return quote_results
