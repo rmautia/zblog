59548843e9283787d6ad0e987fd726d249eda8d8
rmautia <raphaelnyangenya@gmail.com>
2020-05-12 19:08:23 +0300

    add live link

--- app/auth/views.py
+++ app/auth/views.py
@@ -17,7 +17,7 @@ def login():
 
         flash('Invalid username or Password')
 
-    title = "Jabulani - Log In"
+    title = "Log In"
 
     return render_template('auth/login.html',login_form = login_form,title=title)
 
