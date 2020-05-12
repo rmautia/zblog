91670cf517afd153c95861ee42d982f2c3fcef5c
rmautia <raphaelnyangenya@gmail.com>
2020-05-12 13:57:12 +0300

    deployment to heroku

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 export QUOTES_API_URL=http://quotes.stormconsultancy.co.uk/random.json
 export MAIL_USERNAME=raphaelnyangenya@gmail.com
 export MAIL_PASSWORD=Raphaelite2019.
+export SECRET_KEY=ranayapostblog
 
 python3.7 manage.py server
